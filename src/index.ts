ae747af38581bdbf574ca676fdc9325106ee6cfa
rtardo <84642031+rtardo@users.noreply.github.com>
2024-12-20 08:36:39 +0300

    Update session middleware and CORS settings

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
+/*
+import dotenv from 'dotenv';
+dotenv.config();
+*/
 import cors from 'cors';
 import MongooseService from './common/initdb';
 
@@ -14,6 +18,7 @@ import RedisStore from 'connect-redis';
 import session from 'express-session';
 declare module 'express-session' {
     interface SessionData {
+        //user: { [key: string]: any };
         _id: string;
     }
   }
@@ -24,23 +29,37 @@ import { AuthRoutes } from './auth/auth.routes';
 import { UsersRoutes } from './users/users.routes';
 import { RoomsRoutes } from './rooms/rooms.routes';
 import path from 'path';
-
-
+/*
+const dotenvResult = dotenv.config();
+if (dotenvResult.error) {
+    throw dotenvResult.error;
+}
+*/
 export const app: express.Application = express();
-
+//socket
 const httpServer = createServer(app);
-const io = new Server(httpServer);
-
-const port = process.env.PORT || 3000;
+const io = require("socket.io")(httpServer, {
+    cors: {
+      origin: true,
+      credentials: true
+    }
+  });
+//socket
+const port = 3000;
 const routes: Array<CommonRoutes> = [];
 const debugLog: debug.IDebugger = debug('app');
 
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
-app.use(cors());
-
+let corsOptions = {
+    credentials: true,
+    origin: true
+ }
+ 
+ app.use(cors(corsOptions))
+//socket
 app.use(express.static(path.join(__dirname, 'public')));
-
+//socket
 const loggerOpts: expressWinston.LoggerOptions = {
     transports: [new winston.transports.Console()],
     format: winston.format.combine(
@@ -56,27 +75,37 @@ if (String(process.env.STAGE) !== 'DEV') {
 
 app.use(expressWinston.logger(loggerOpts));
 
+//app.set('trust proxy', 1);
+
+// Initialize client.
 const redisClient = createClient({
     url: String(process.env.CACHE_URL)
   });
 
 redisClient.connect().catch(console.error);
 
+// Initialize store.
 const redisStore = new RedisStore({
     client: redisClient,
 });
 
+app.set('trust proxy', (ip) => {
+    if (ip === '127.0.0.1' || ip === '123.123.123.123') return true // trusted IPs
+    else return false
+  })
+
+// Initialize sesssion storage.
 app.use(
     session({
         store: redisStore,
-        saveUninitialized: false,
+        saveUninitialized: true,
         secret: String(process.env.SESSION_SECRET),
         resave: false,
         name: 'sessionId',
         cookie: {
             secure: false,
             httpOnly: true,
-            sameSite: 'lax',
+            sameSite: 'none',
             maxAge: 1000 * 60 * 30,
         },
     })
@@ -84,7 +113,9 @@ app.use(
 
 routes.push(new AuthRoutes(app));
 routes.push(new UsersRoutes(app));
+//socket
 routes.push(new RoomsRoutes(app));
+//socket
 
 const runningMsg = `Server running at http://localhost:${port}`;
 
@@ -101,7 +132,16 @@ function errorHandler(error: any, req: express.Request, res: express.Response, n
 }
 
 app.use(errorHandler);
+/*
+app.listen(port, () => {
+    routes.forEach((route: CommonRoutes) => {
+        debugLog(`Routes configured for ${route.getName()}`);
+    });
 
+    console.log(runningMsg);
+});
+*/
+//socket
 httpServer.listen(port, () => {
     MongooseService.connectWithRetry();
     routes.forEach((route: CommonRoutes) => {
@@ -110,7 +150,9 @@ httpServer.listen(port, () => {
 
     console.log(runningMsg);
 });
+//socket
 
+//socket
 io.on('connection', (socket) => {
     console.log('user connected');
 
@@ -118,6 +160,8 @@ io.on('connection', (socket) => {
         console.log('new-user');
         console.log('new-user', socketId);
         socket.join(chatroomId);
+        //rooms[room].users[socket.id] = name
+        //socket.to(chatroomId).local.emit('user-connected', userId);
         socket.broadcast.to(chatroomId).emit('user-connected', chatroomId, userId, socketId);
     });
 
@@ -143,3 +187,4 @@ io.of('/').adapter.on('create-room', (room) => {
 io.of('/').adapter.on('join-room', (room, id) => {
     console.log(`socket ${id} has joined room ${room}`);
 });
+//socket
