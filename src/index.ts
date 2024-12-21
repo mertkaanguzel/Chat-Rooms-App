15057685cd23bdc6e895c94ce3da736ab60ecfd0
Mert Kaan Güzel <84642031+mertkaanguzel@users.noreply.github.com>
2024-12-21 00:28:02 +0000

    Enable trust proxy

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,29 @@
 import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
-/*
-import * as dotenv from "dotenv";
-dotenv.config({ path: __dirname+'/.env' });
-*/
-
 import cors from 'cors';
 import MongooseService from './common/initdb';
-
 import * as winston from 'winston';
 import * as expressWinston from 'express-winston';
-
 import debug from 'debug';
-
-
 import RedisStore from 'connect-redis';
 import session from 'express-session';
 declare module 'express-session' {
     interface SessionData {
-        //user: { [key: string]: any };
         _id: string;
     }
   }
 import { createClient } from 'redis';
-
 import { CommonRoutes } from './common/common.routes';
 import { AuthRoutes } from './auth/auth.routes';
 import { UsersRoutes } from './users/users.routes';
 import { RoomsRoutes } from './rooms/rooms.routes';
 import path from 'path';
-/*
-const dotenvResult = dotenv.config();
-if (dotenvResult.error) {
-    throw dotenvResult.error;
-}
-*/
+
 export const app: express.Application = express();
-//socket
+
+app.set('trust proxy', 1);
+
 const httpServer = createServer(app);
 const io = require("socket.io")(httpServer, {
     cors: {
@@ -45,22 +31,22 @@ const io = require("socket.io")(httpServer, {
       credentials: true
     }
   });
-//socket
+
 const port = process.env.PORT;
 const routes: Array<CommonRoutes> = [];
 const debugLog: debug.IDebugger = debug('app');
 
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
+
 let corsOptions = {
     credentials: true,
-    origin: true
+    origin: 'https://chat-rooms-app-production.up.railway.app',
  }
- 
  app.use(cors(corsOptions))
-//socket
+
 app.use(express.static(path.join(__dirname, 'public')));
-//socket
+
 const loggerOpts: expressWinston.LoggerOptions = {
     transports: [new winston.transports.Console()],
     format: winston.format.combine(
@@ -69,16 +55,11 @@ const loggerOpts: expressWinston.LoggerOptions = {
         winston.format.colorize({ all: true }),
     ),
 };
-
 if (String(process.env.STAGE) !== 'DEV') {
     loggerOpts.meta = false;
 }
-
 app.use(expressWinston.logger(loggerOpts));
 
-//app.set('trust proxy', 1);
-
-// Initialize client.
 const redisClient = createClient({
     username: String(process.env.CACHE_USERNAME),
     password: String(process.env.CACHE_PASSWORD),
@@ -87,26 +68,16 @@ const redisClient = createClient({
         port: Number(process.env.CACHE_PORT),
     }
   });
-
 redisClient.connect().catch(console.error);
-
-// Initialize store.
 const redisStore = new RedisStore({
     client: redisClient,
 });
 
 
-
-app.set('trust proxy', (ip) => {
-    if (ip === '127.0.0.1' || ip === '123.123.123.123') return true // trusted IPs
-    else return false
-  })
-
-// Initialize sesssion storage.
 app.use(
     session({
         store: redisStore,
-        saveUninitialized: true,
+        saveUninitialized: false,
         secret: String(process.env.SESSION_SECRET),
         resave: false,
         name: 'sessionId',
@@ -121,12 +92,10 @@ app.use(
 
 routes.push(new AuthRoutes(app));
 routes.push(new UsersRoutes(app));
-//socket
 routes.push(new RoomsRoutes(app));
-//socket
+
 
 const runningMsg = `Server running at http://localhost:${port}`;
-
 app.get('/', (req: express.Request, res: express.Response) => {
     res.status(200).send(runningMsg);
 });
@@ -138,18 +107,8 @@ function errorHandler(error: any, req: express.Request, res: express.Response, n
         error: error.message,
     }));
 }
-
 app.use(errorHandler);
-/*
-app.listen(port, () => {
-    routes.forEach((route: CommonRoutes) => {
-        debugLog(`Routes configured for ${route.getName()}`);
-    });
 
-    console.log(runningMsg);
-});
-*/
-//socket
 httpServer.listen(port, () => {
     MongooseService.connectWithRetry();
     routes.forEach((route: CommonRoutes) => {
@@ -158,9 +117,9 @@ httpServer.listen(port, () => {
 
     console.log(runningMsg);
 });
-//socket
 
-//socket
+
+
 io.on('connection', (socket) => {
     console.log('user connected');
 
@@ -168,8 +127,6 @@ io.on('connection', (socket) => {
         console.log('new-user');
         console.log('new-user', socketId);
         socket.join(chatroomId);
-        //rooms[room].users[socket.id] = name
-        //socket.to(chatroomId).local.emit('user-connected', userId);
         socket.broadcast.to(chatroomId).emit('user-connected', chatroomId, userId, socketId);
     });
 
@@ -189,10 +146,11 @@ io.on('connection', (socket) => {
         console.log('LIST OF ROOMS:', socket.id);
     });
 });
+
 io.of('/').adapter.on('create-room', (room) => {
     console.log(`room ${room} was created`);
 });
+
 io.of('/').adapter.on('join-room', (room, id) => {
     console.log(`socket ${id} has joined room ${room}`);
 });
-//socket
